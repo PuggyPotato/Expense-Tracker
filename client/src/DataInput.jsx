82a5b9547fc2dd63a940338910f2ed8e847d5e18
PuggyPotato <qingyaurou@gmail.com>
2025-03-28 23:08:26 +0800

    Added backend dependencies, added a function to handle submit in the frontend

--- client/src/DataInput.jsx
+++ client/src/DataInput.jsx
@@ -6,9 +6,13 @@ import { useState } from "react"
 
 
 function DataInput(){
-    const [category,setCategory] = useState("FoodDrinks")    
-    const [amount,setAmount] = useState("")
+    const [category,setCategory] = useState("FoodDrinks");    
+    const [amount,setAmount] = useState("");
 
+
+    function handleSubmit(event){
+        event.preventDefault();
+    } 
 
 
 
@@ -16,16 +20,18 @@ function DataInput(){
     return(
         <>
             <div>
-                <input type="number" onChange={(e) =>setAmount(e.target.value)} value={category} placeholder="Enter Price Here..."></input>
-                <select onChange={(e) => setCategory(e.target.value)}>
-                    <option value={"FoodDrinks"}>Food / Drinks</option>
-                    <option value={"Entertainment"}>Entertainment</option>
-                    <option value={"Bills"}>Bills</option>
-                    <option value={"Miscellaneous"}>Miscellaneous</option>
-                    <option value={"Medical"}>Health And Medical</option>
+                <form onSubmit={handleSubmit}>
+                    <input type="number" onChange={(e) =>setAmount(e.target.value)} value={category} placeholder="Enter Price Here..."></input>
+                    <select onChange={(e) => setCategory(e.target.value)}>
+                        <option value={"FoodDrinks"}>Food / Drinks</option>
+                        <option value={"Entertainment"}>Entertainment</option>
+                        <option value={"Bills"}>Bills</option>
+                        <option value={"Miscellaneous"}>Miscellaneous</option>
+                        <option value={"Medical"}>Health And Medical</option>
 
-                </select>
-                <button>Upload </button>
+                    </select>
+                    <button type="submit">Upload </button>
+                </form>
             </div>
         </>
     )
